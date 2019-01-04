d620e3b584bf8b37c96369d5b971d5fdf58a8ec2
nasim <nasim_ebi@yahoo.com>
2019-01-04 15:54:37 +0100

    final exercice

--- exercice_vendredi.rb
+++ exercice_vendredi.rb
@@ -53,10 +53,10 @@ puts " #exercice 06 ------------------------------------------------------"
 puts "enter l'année de ta naissance "
 print ">"
 a = gets.chomp
-for i in a.to_i..2018 do
-   puts  "en" + " #{i}" 
-   puts "  tu avais #{ i- a.to_i} ans"
-end 
+	for i in a.to_i..2018 do
+   		puts  "en" + " #{i}" 
+  		 puts "  tu avais #{ i- a.to_i} ans"
+	end 
 
 puts " #exercice 07 ------------------------------------------------------"
 
@@ -67,9 +67,9 @@ ans = 0
 
 		while age > 0
 		puts "il y a #{ans+1} ans tu avais #{age-1} ans !"
- 		ans= ans+1
- 		age= age-1
- 	end 
+ 	ans= ans+1
+ 	age= age-1
+	 end 
 
 
 puts " #exercice 08 ------------------------------------------------------"
@@ -91,4 +91,74 @@ ans = 0
  	end 
 
 puts " #exercice 09 ------------------------------------------------------"
+
+emails = []
+
+50.times do |i|
+    emails << "jean.dupont.0#{i + 1}@email.fr"
+
+  end
+
+puts emails
+
+
+puts " #exercice 10 ------------------------------------------------------"
+
+emails = []
+
+50.times do |i|
+    emails << "jean.dupont.0#{i + 1}@email.fr"
+
+  end
+
+
+emails.length.times do |i|
+  if i % 2 == 1
+
+  puts emails[i]
+
+  end
+
+end
+
+
+puts " #exercice 11 ------------------------------------------------------"
+
+
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 0-25)"
+print ">"
+
+etag = gets.chomp
+pyramide = []
+etage.to_i.times do |i|
+    pyramide << "#{"#"*(i + 1)}"
+  end
+
+puts "Voici la pyramide:"
+
+  if etage.to_i < 26
+    puts pyramide
+
+end
+
+
+puts " #exercice 12 ------------------------------------------------------"
+
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu?"
+print ">"
+
+etage = gets.chomp
+pyramide = []
+	a = etage.to_i
+	a.times do |i|
+	
+	symbole = "#{"#" * i}"
+	space = "#{" " * (a - i)}"
+	pyramide << space + symbole
+end
+
+	if a <= 25
+		puts pyramide
+end
 
