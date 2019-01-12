120a55844c5c2d8937797dcd7229a5948e5e004c
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-01-12 22:20:46 +0100

    odd edited

--- odd or even exerce.rb
+++ odd or even exerce.rb
@@ -1,4 +1,5 @@
 puts " Enter un odd numbre plz :) "
+puts " just for joking you can enter un even number "
 num = gets.chomp.to_i
 count =1
 while num.even?
@@ -18,43 +19,4 @@ end
 end 
 puts " the numbre that you have enterd is *  #{num}   *" 
 puts " you have tried #{count} times"
-
-
-puts " game number two ******************************************************************"
-puts "                                                                             "
-puts "                                                                             "
-puts "                                                                             "
-puts "                                                                             "
-puts "                                                                             "
-puts "  You should find a number between 0-10 which have selected by computer "
-puts "  attention you have 4 choice :) Read carefully the direction " 
-puts "  Bonne chance"
-
-number = rand(10) 
-for period in 1..4 
-	a = nil 
-	next if a == number
-	puts "enter a numbre !"
-	print ">"
-a = gets.chomp.to_i
-	if a > number 
-		puts "  Big ! try with the smallest  one"
-	elsif a < number 
-		puts " small ! try with the biggest one"
-	end 
-end
-	puts  "     your are so smart    "
-	puts  " *************************"
-	puts  " *************************"
-	puts  " **                     **"
-	puts  " **       BRAVO         **"
-	puts  " **                     **"
-	puts  " *************************"
-	puts  " *************************"
-if a != number
-puts " you lose :( "
-end 
-puts "the number which was selected is #{number}"
-puts " thanks for trying this game"
-		
 
