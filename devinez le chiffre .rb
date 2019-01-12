43399a33538dc9e84fc00f8636b43759e65e3dcb
n-ibrahimi <nasim.hamdard21@gmail.com>
2019-01-12 21:08:26 +0100

    game 2

---
+++ devinez le chiffre .rb
@@ -0,0 +1,42 @@
+
+puts "                             wellcome                                        "
+puts "                                                                             "
+puts "  You should find a number between 0-10 which have selected by computer "
+puts "  attention you have 3 choice :) Read carefully the direction " 
+puts "  Bonne chance"
+
+number = rand(10) 
+a = nil 
+for period in 1..3 
+	next if a == number
+	puts  "    Enter a numbre !"
+	print "    >"
+a = gets.chomp.to_i
+	if a > number 
+		puts "  Big ! try with the smallest  one"
+	elsif a < number 
+		puts "  small ! try with the biggest one"
+	end 
+end
+if a==number
+	puts  "     your are so smart    "
+	puts  " *************************"
+	puts  " *************************"
+	puts  " **                     **"
+	puts  " **       BRAVO         **"
+	puts  " **                     **"
+	puts  " *************************"
+	puts  " *************************"
+elsif 
+	puts  " *************************"
+	puts  " *************************"
+	puts  " **                     **"
+	puts  " **     You lose        **"
+	puts  " **                     **"
+	puts  " *************************"
+	puts  " *************************"
+end 
+puts "the number which was selected is #{number}"
+puts " thanks for trying this game"
+		
+
--- odd or even exerce.rb
+++ odd or even exerce.rb
@@ -5,6 +5,7 @@ while num.even?
 	puts "Enter un **odd** numbre (^^) "
 	count +=1
 	num = gets.chomp.to_i
+end 
 	if num.odd?
 	puts  "---------  yes  ----------"
 	puts  " *************************"
@@ -14,9 +15,46 @@ while num.even?
 	puts  " **                     **"
 	puts  " *************************"
 	puts  " *************************"
-	end
 end 
 puts " the numbre that you have enterd is *  #{num}   *" 
 puts " you have tried #{count} times"
 
+
+puts " game number two ******************************************************************"
+puts "                                                                             "
+puts "                                                                             "
+puts "                                                                             "
+puts "                                                                             "
+puts "                                                                             "
+puts "  You should find a number between 0-10 which have selected by computer "
+puts "  attention you have 4 choice :) Read carefully the direction " 
+puts "  Bonne chance"
+
+number = rand(10) 
+for period in 1..4 
+	a = nil 
+	next if a == number
+	puts "enter a numbre !"
+	print ">"
+a = gets.chomp.to_i
+	if a > number 
+		puts "  Big ! try with the smallest  one"
+	elsif a < number 
+		puts " small ! try with the biggest one"
+	end 
+end
+	puts  "     your are so smart    "
+	puts  " *************************"
+	puts  " *************************"
+	puts  " **                     **"
+	puts  " **       BRAVO         **"
+	puts  " **                     **"
+	puts  " *************************"
+	puts  " *************************"
+if a != number
+puts " you lose :( "
+end 
+puts "the number which was selected is #{number}"
+puts " thanks for trying this game"
+		
 
